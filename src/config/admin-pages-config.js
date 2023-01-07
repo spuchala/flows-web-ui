00cef7cf811eaeee1e6243d488be5542183c91c0
Rakesh Nune <nunerakesh@gmail.com>
2023-01-07 14:54:36 -0500

    Changes to main branch

--- src/config/admin-pages-config.js
+++ src/config/admin-pages-config.js
@@ -1,16 +1,25 @@
 const adminPagesConfig = [
   {
+    // This is the configuration for the "Flows" page
     title: "Flows",
     route: "/flows"
   },
   {
-    title: "Departments",
-    route: "/departments"
+    // This is the configuration for the "HOME" page
+    title: "HOME",
+    route: "/home"
   },
   {
-    title: "People",
-    route: "/people"
+    // This is the configuration for the "Help" page
+    title: "Help",
+    route: "/help"
+  },
+  {
+    // This is the configuration for the "Login" page
+    title: "Login",
+    route: "/login"
   }
 ];
 
+// This exports the configuration for the admin pages
 export default adminPagesConfig;
