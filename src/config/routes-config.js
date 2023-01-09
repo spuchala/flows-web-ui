aec3e147e96d731665a7ba248b9ca1d9917ed9cf
s0p00p5 <sankar.puchala@walmart.com>
2023-01-09 15:08:38 -0500

    set up about page

--- src/config/routes-config.js
+++ src/config/routes-config.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../views/home/home";
+import About from "../views/about/about";
 
 const RoutesConfig = () => {
   return (
@@ -11,6 +12,7 @@ const RoutesConfig = () => {
       <Route exact path="/flows-web-ui" element={<Home />} />
       <Route exact path="/departments" element={<Home />} />
       <Route exact path="/people" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
     </Routes>
   );
 };
