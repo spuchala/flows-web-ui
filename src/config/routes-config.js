942ddea0eec1543372290ba7ba95f7319fa63c2d
s0p00p5 <sankar.puchala@walmart.com>
2023-02-04 15:44:11 -0500

    home page setup

--- src/config/routes-config.js
+++ src/config/routes-config.js
@@ -3,17 +3,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Flows from "../views/flows/flows";
 import About from "../views/about/about";
+import Home from "../views/home/home";
 
 const RoutesConfig = () => {
   return (
     <Routes>
       <Route exact path="/" element={<About />} />
-      {/* This is the main landing page  */}
       <Route exact path="/flows" element={<Flows />} />
       <Route exact path="/flows-web-ui" element={<About />} />
-      {/* <Route exact path="/departments" element={<About />} /> */}
-      {/* <Route exact path="/people" element={<Home />} /> */}
       <Route exact path="/about" element={<About />} />
+      <Route exact path="/home" element={<Home />} />
     </Routes>
   );
 };
---
+++ src/views/home/home.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Container, Typography, Grid, Box } from "@mui/material";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <>
+      <section className="mainSection">
+        <Container maxWidth="lg" className="mainContainer">
+          <img
+            style={{ display: "none" }}
+            src="https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&amp;fit=crop&amp;w=1400"
+            alt="increase priority"
+          />
+          <Typography variant="h3" gutterBottom>
+            MAIN HEADING GOES HERE
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            Sub Heading Goes Here
+          </Typography>
+        </Container>
+      </section>
+      <section className="featuresSection">
+        <Container className="featuresContainer">
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={4} md={4}>
+                <AcUnitIcon />
+                <Typography>
+                  Feature One description goes here. Change the icon above this
+                  by googling MUI Icons and replace the icon above.
+                </Typography>
+              </Grid>
+              <Grid item xs={4} md={4}>
+                <AcUnitIcon />
+                <Typography>
+                  Feature Two description goes here. Change the icon above this
+                  by googling MUI Icons and replace the icon above.
+                </Typography>
+              </Grid>
+              <Grid item xs={4} md={4}>
+                <AcUnitIcon />
+                <Typography>
+                  Feature Three description goes here. Change the icon above
+                  this by googling MUI Icons and replace the icon above.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Home;
